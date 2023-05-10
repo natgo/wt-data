e7567ef38ea47871161ab373f664b0367b059d21
natgo <xenhud@protonmail.com>
2023-05-10 19:35:18 +0300

    Remove top speed from wiki.json

--- types/wiki.schema.ts
+++ types/wiki.schema.ts
@@ -1,16 +1,10 @@
 import { z } from "zod";
 
-// Wiki
-export const topSpeed = z.number().array().length(2);
-export type TopSpeed = z.infer<typeof topSpeed>;
-
 export const visibility = z.number().max(3000);
 export type Visibility = z.infer<typeof visibility>;
 
 export const wikiSigleSchema = z.object({
   intname: z.string(),
-  ab_top_speed: topSpeed,
-  rb_top_speed: topSpeed,
   visibility: visibility,
 });
 export type WikiSigle = z.infer<typeof wikiSigleSchema>;
